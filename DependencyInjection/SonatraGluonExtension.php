1c3b82b8ccba51e3f14298f19f00731ab3b4053d
François Pluchino <francois.pluchino@sonatra.com>
2013-12-21 18:29:02 +0100

    Adds fixed debug toolbar

--- DependencyInjection/SonatraGluonExtension.php
+++ DependencyInjection/SonatraGluonExtension.php
@@ -31,6 +31,7 @@ class SonatraGluonExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('response.xml');
     }
 }
---
+++ EventListener/WebDebugToolbarListener.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\GluonBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+
+/**
+* WebDebugToolbarListener injects the javascript edit Web Debug Toolbar.
+*
+* The onKernelResponse method must be connected to the kernel.response event.
+*
+* @author François Pluchino <francois.pluchino@sonatra.com>
+*/
+class WebDebugToolbarListener
+{
+    const ENV_ENABLED = 'dev';
+
+    protected $templating;
+    protected $env;
+
+    /**
+     * Constroctor of Class.
+     *
+     * @param TwigEngine $templating A Twig object of template.
+     * @param String     $env        A type of environment (dev, prod, etc...).
+     */
+    public function __construct(TwigEngine $templating, $env = self::ENV_ENABLED)
+    {
+        $this->templating = $templating;
+        $this->env = (string) $env;
+    }
+
+    /**
+     * Method for a dependency injection.
+     *
+     * @param FilterResponseEvent $event A event object.
+     */
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $request = $event->getRequest();
+
+        // do not capture redirects or modify XML HTTP Requests
+        if ($request->isXmlHttpRequest()) {
+            return;
+        }
+
+        if (self::ENV_ENABLED != $this->env
+            || !$response->headers->has('X-Debug-Token')
+            || '3' === substr($response->getStatusCode(), 0, 1)
+            || ($response->headers->has('Content-Type') && false === strpos($response->headers->get('Content-Type'), 'html'))
+            || 'html' !== $request->getRequestFormat()
+        ) {
+            return;
+        }
+
+        $this->injectEditToolbar($response);
+    }
+
+    /**
+     * Injects the javavascript edit web debug toolbar into the given Response.
+     *
+     * @param Response $response A Response instance
+     */
+    protected function injectEditToolbar(Response $response)
+    {
+        if (function_exists('mb_stripos')) {
+            $posrFunction = 'mb_strripos';
+            $substrFunction = 'mb_substr';
+
+        } else {
+            $posrFunction = 'strripos';
+            $substrFunction = 'substr';
+        }
+
+        $content = $response->getContent();
+
+        if (false !== $pos = $posrFunction($content, '</body>')) {
+            $toolbar = "\n".str_replace("\n", '', $this->templating->render(
+                'SonatraGluonBundle:Profiler:toolbar_js.html.twig',
+                array('token' => $response->headers->get('X-Debug-Token'))
+                ))."\n";
+            $content = $substrFunction($content, 0, $pos).$toolbar.$substrFunction($content, $pos);
+            $response->setContent($content);
+        }
+    }
+}
