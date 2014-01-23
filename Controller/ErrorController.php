b79692dc3f190f0d3c00cf64cb91183d89d218fa
François Pluchino <francois.pluchino@sonatra.com>
2014-01-23 18:32:54 +0100

    Adds error page template

---
+++ Controller/ErrorController.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\GluonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Error controller.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class ErrorController extends Controller
+{
+    /**
+     * Display page error for dev.
+     */
+    public function showAction($code)
+    {
+        $status = Response::$statusTexts;
+        $text = isset($status[$code]) ? $status[$code] : null;
+
+        return $this->render('@SonatraGluon/Exception/error.html.twig', array('status_code' => $code, 'status_text' => $text));
+    }
+}
