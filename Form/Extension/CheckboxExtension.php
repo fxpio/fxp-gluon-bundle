52b511ffda634470566d2b1757fc3cd07b139ef2
François Pluchino <francois.pluchino@sonatra.com>
2014-01-31 14:07:25 +0100

    Adds style option on checkbox and radio form

---
+++ Form/Extension/CheckboxExtension.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\GluonBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Checkbox Form Extension.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class CheckboxExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars = array_replace($view->vars, array(
+            'style' => $options['style'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'style' => null,
+        ));
+
+        $resolver->setAllowedTypes(array(
+            'style' => array('null', 'string'),
+        ));
+
+        $resolver->setAllowedValues(array(
+            'style' => array('default', 'primary', 'secondary', 'success', 'info', 'warning', 'danger', 'link'),
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'checkbox';
+    }
+}
