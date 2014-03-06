c241fd3fb25c6a35cf5a91649b41471911aaf921
François Pluchino <francois.pluchino@sonatra.com>
2014-03-06 19:04:54 +0100

    Removes unused class

--- Block/Type/NavbarScrollableType.php
+++ Block/Type/NavbarScrollableType.php
@@ -14,7 +14,6 @@ namespace Sonatra\Bundle\GluonBundle\Block\Type;
 use Sonatra\Bundle\BlockBundle\Block\AbstractType;
 use Sonatra\Bundle\BlockBundle\Block\BlockView;
 use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\OptionsResolver\Options;
 
 /**
