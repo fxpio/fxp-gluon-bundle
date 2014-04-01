021050e5cc10261f376385eb89793824e612d0e9
François Pluchino <francois.pluchino@sonatra.com>
2014-04-01 21:12:34 +0200

    Adds panel actions block

---
+++ Block/Type/PanelActionsType.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\GluonBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
+use Sonatra\Bundle\BlockBundle\Block\Exception\InvalidConfigurationException;
+use Sonatra\Bundle\GluonBundle\Block\Util\PanelUtil;
+
+/**
+ * Panel Actions Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class PanelActionsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function addParent(BlockInterface $parent, BlockInterface $block, array $options)
+    {
+        if (!PanelUtil::isValidBlock('panel_header', $parent)) {
+            $msg = 'The "panel_actions" parent block (name: "%s") must be a "panel_header" block type';
+            throw new InvalidConfigurationException(sprintf($msg, $block->getName()));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'panel_actions';
+    }
+}
