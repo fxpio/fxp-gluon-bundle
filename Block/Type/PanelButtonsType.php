1b90de2cd2935beccc17ffe5b39736b61d7e3502
François Pluchino <francois.pluchino@sonatra.com>
2014-04-04 12:52:57 +0200

    Adds nav scrollable block in panel buttons builder

--- Block/Type/PanelButtonsType.php
+++ Block/Type/PanelButtonsType.php
@@ -12,6 +12,7 @@
 namespace Sonatra\Bundle\GluonBundle\Block\Type;
 
 use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+use Sonatra\Bundle\BlockBundle\Block\BlockBuilderInterface;
 use Sonatra\Bundle\BlockBundle\Block\BlockView;
 use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
 use Sonatra\Bundle\BlockBundle\Block\Exception\InvalidConfigurationException;
@@ -28,9 +29,56 @@ class PanelButtonsType extends AbstractType
     /**
      * {@inheritdoc}
      */
+    public function buildBlock(BlockBuilderInterface $builder, array $options)
+    {
+        if ($options['scrollable']) {
+            $builder->add('_nav_scrollable', 'nav_scrollable');
+            $builder->get('_nav_scrollable')->add('_navButtonGroup', 'block', array(
+                'attr' => array(
+                    'class' => $options['class_nav'],
+                ),
+            ));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function addChild(BlockInterface $child, BlockInterface $block, array $options)
     {
-        if (!BlockUtil::isValidBlock(array('button', 'nav_scrollable'), $child)) {
+        // scrollable
+        if (BlockUtil::isValidBlock('nav_scrollable', $child)) {
+            if ($block->getAttribute('already_nav_scrollable')) {
+                $navScrollable = $block->get($block->getAttribute('already_nav_scrollable'));
+
+                foreach ($navScrollable->all() as $name => $action) {
+                    $child->add($action);
+                }
+
+                $block->remove($block->getAttribute('already_nav_scrollable'));
+
+            } else {
+                $block->setAttribute('already_nav_scrollable', $child->getName());
+            }
+
+            $attr = $child->getOption('attr');
+            $attr['data-class-nav'] = $options['class_nav'];
+            $child->setOption('attr', $attr);
+
+        // button
+        } elseif (BlockUtil::isValidBlock('button', $child)) {
+            $parent = $this->findParentButtons($block);
+
+            if ($parent !== $block) {
+                $parent->add($child);
+            }
+
+            if (null === $child->getOption('size')) {
+                $child->setOption('size', $options['button_size']);
+            }
+
+        // other
+        } else {
             $msg = 'The "panel_buttons" child block (name: "%s") must be a "button" block type';
             throw new InvalidConfigurationException(sprintf($msg, $child->getName()));
         }
@@ -49,39 +97,18 @@ class PanelButtonsType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function finishView(BlockView $view, BlockInterface $block, array $options)
-    {
-        $buttons = array();
-        $scrollable = null;
-
-        foreach ($view->children as $name => $child) {
-            if (in_array('button', $child->vars['block_prefixes'])) {
-                $buttons[] = $child;
-
-            } elseif (null === $scrollable && in_array('nav_scrollable', $child->vars['block_prefixes'])) {
-                $scrollable = $child;
-            }
-        }
-
-        if (null !== $scrollable) {
-            $scrollable->vars['attr']['data-class-nav'] = 'nav-btn-group';
-
-        } elseif ($options['scrollable']) {
-            var_dump('ici');
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'scrollable' => true,
+            'scrollable'  => true,
+            'class_nav'   => 'nav-btn-group',
+            'button_size' => 'sm',
         ));
 
         $resolver->setAllowedTypes(array(
-            'scrollable' => 'bool',
+            'scrollable'  => 'bool',
+            'class_nav'   => 'string',
+            'button_size' => 'string',
         ));
     }
 
@@ -91,5 +118,39 @@ class PanelButtonsType extends AbstractType
     public function getName()
     {
         return 'panel_buttons';
+    }
+
+    /**
+     * Finds the parent block of buttons.
+     *
+     * @param BlockInterface $block
+     *
+     * @return BlockInterface
+     */
+    protected function findParentButtons(BlockInterface $block)
+    {
+        if ($block->getAttribute('block_parent_buttons')) {
+            return $block->getAttribute('block_parent_buttons');
+        }
+
+        $parent = $block;
+
+        if ($block->getOption('scrollable')) {
+            foreach ($block->all() as $name => $child) {
+                if (BlockUtil::isValidBlock('nav_scrollable', $child)) {
+                    foreach ($child->all() as $name => $subChild) {
+                        if (BlockUtil::isValidBlock('block', $subChild)) {
+                            $parent = $subChild;
+                            break;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        $block->setAttribute('block_parent_buttons', $parent);
+
+        return $parent;
     }
 }
