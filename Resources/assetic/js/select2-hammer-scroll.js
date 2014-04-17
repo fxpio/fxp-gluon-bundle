0af43824bcef0f23b45a313c420a6eb217174dfd
François Pluchino <francois.pluchino@sonatra.com>
2014-04-17 17:59:14 +0200

    Moves the override code of select2 to the good place

--- Resources/assetic/js/select2-hammer-scroll.js
+++ Resources/assetic/js/select2-hammer-scroll.js
@@ -32,14 +32,6 @@
         this.$element.on('select2-close.st.select2hammerscroll', $.proxy(onClose, this));
         this.$element.on('select2-loaded.st.select2hammerscroll', $.proxy(onLoaded, this));
 
-        var select2 = this.$element.data('select2');
-
-        if (!select2.opts.multiple) {
-            select2.selection.off('mousedown touchstart');
-            select2.selection.on("mousedown touchend", 'abbr', $.proxy(onSelect2Clear, select2));
-            select2.selection.on("mousedown touchend", $.proxy(onSelect2Open, select2));
-        }
-
         // force use scroll
         this.options.useScroll = true;
     };
@@ -63,11 +55,6 @@
         var select2 = this.$element.data('select2');
         var $dropdown = $(select2.dropdown);
 
-        if (!select2.opts.multiple) {
-            select2.selection.off("mousedown touchend", 'abbr', $.proxy(onSelect2Clear, select2));
-            select2.selection.off("mousedown touchend", $.proxy(onSelect2Open, select2));
-        }
-
         $dropdown.off('DOMMouseScroll.st.select2hammerscroll mousewheel.st.select2hammerscroll touchmove.st.select2hammerscroll', $.proxy(blockEvent, this));
         this.$element.off('select2-open.st.select2hammerscroll', $.proxy(onOpen, this));
         this.$element.off('select2-close.st.select2hammerscroll', $.proxy(onClose, this));
@@ -79,53 +66,6 @@
 
         this.$element.removeData('st.select2hammerscroll');
     };
-
-    /**
-     * Action on clear select2 value.
-     *
-     * @param jQuery.Event event
-     *
-     * @this (is select2 instance)
-     * @private
-     */
-    function onSelect2Clear (event) {
-        if (!this.isInterfaceEnabled()) return;
-        this.clear();
-        event.preventDefault();
-        event.stopImmediatePropagation();
-        this.close();
-        this.selection.focus();
-    }
-
-    /**
-     * Action on open select2 dropdown.
-     *
-     * @param jQuery.Event event
-     *
-     * @this (is select2 instance)
-     * @private
-     */
-    function onSelect2Open (event) {
-        // Prevent IE from generating a click event on the body
-        var placeholder = $(document.createTextNode(''));
-
-        this.selection.before(placeholder);
-        placeholder.before(this.selection);
-        placeholder.remove();
-
-        if (!this.container.hasClass("select2-container-active")) {
-            this.opts.element.trigger($.Event("select2-focus"));
-        }
-
-        if (this.opened()) {
-            this.close();
-        } else if (this.isInterfaceEnabled()) {
-            this.open();
-        }
-
-        event.preventDefault();
-        event.stopPropagation();
-    }
 
     /**
      * Action on opened select2 dropdown.
