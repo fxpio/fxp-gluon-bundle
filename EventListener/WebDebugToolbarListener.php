6fc2d24a78e1aea043cb3fb03e39d8b3267ee63e
François Pluchino <francois.pluchino@sonatra.com>
2014-08-08 10:11:20 +0200

    Remove unused code

--- EventListener/WebDebugToolbarListener.php
+++ EventListener/WebDebugToolbarListener.php
@@ -11,7 +11,6 @@
 
 namespace Sonatra\Bundle\GluonBundle\EventListener;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
