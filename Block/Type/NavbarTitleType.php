acd74e1654b91dc4fff6cd72cf162cce509dfd41
François Pluchino <francois.pluchino@sonatra.com>
2015-09-11 02:18:33 +0200

    Add navbar title block

---
+++ Block/Type/NavbarTitleType.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\GluonBundle\Block\Type;
+
+use Sonatra\Bundle\BlockBundle\Block\AbstractType;
+
+/**
+ * Navbar Title Block Type.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class NavbarTitleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'navbar_title';
+    }
+}
