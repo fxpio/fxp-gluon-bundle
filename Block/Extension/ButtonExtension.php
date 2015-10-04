995789dff1934316191a2ac6e44b0c2504b13192
François Pluchino <francois.pluchino@sonatra.com>
2015-10-04 16:46:12 +0200

    Extract raised option to generic block extension

--- Block/Extension/ButtonExtension.php
+++ Block/Extension/ButtonExtension.php
@@ -12,8 +12,6 @@
 namespace Sonatra\Bundle\GluonBundle\Block\Extension;
 
 use Sonatra\Bundle\BlockBundle\Block\AbstractTypeExtension;
-use Sonatra\Bundle\BlockBundle\Block\BlockInterface;
-use Sonatra\Bundle\BlockBundle\Block\BlockView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -26,24 +24,8 @@ class ButtonExtension extends AbstractTypeExtension
     /**
      * {@inheritdoc}
      */
-    public function buildView(BlockView $view, BlockInterface $block, array $options)
-    {
-        $view->vars = array_replace($view->vars, array(
-            'raised' => $options['raised'],
-        ));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
-            'raised' => false,
-        ));
-
-        $resolver->addAllowedTypes('raised', 'bool');
-
         $resolver->addAllowedValues(array(
             'style' => array('accent', 'navbar'),
         ));
