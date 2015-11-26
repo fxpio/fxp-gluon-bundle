f627ef0cd30456e7be4f5167fde14db23314b262
François Pluchino <francois.pluchino@sonatra.com>
2015-11-26 16:15:03 +0100

    Translate the label of expanded choice items

---
+++ Form/Extension/ChoiceExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Sonatra package.
+ *
+ * (c) François Pluchino <francois.pluchino@sonatra.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonatra\Bundle\GluonBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+/**
+ * Choice Form Extension.
+ *
+ * @author François Pluchino <francois.pluchino@sonatra.com>
+ */
+class ChoiceExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        if (!$options['expanded']) {
+            return;
+        }
+
+        foreach ($view->children as $child) {
+            $child->vars['translation_domain'] = $options['choice_translation_domain'];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return 'choice';
+    }
+}
